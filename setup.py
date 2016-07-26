8876b74404e0e2c64dfc01d02001e3b074063ace
Jason Boutte <boutte.jason@gmail.com>
2016-07-26 14:56:21 -0700

    Removes find_packages as it was not correctly installing files.

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name = 'esgf-compute-api',
     version = '1.0',
-    packages = find_packages('esgf/'),
+    packages = ['esgf'],
 )
