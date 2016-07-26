b01aeff0ce1bf5e3618409994dfd12790dfda740
Jason Boutte <boutte.jason@gmail.com>
2016-07-26 14:16:28 -0700

    Fixes library not correctly being install. Moves OWSLib dependency from setuptools to pip requirements.

--- setup.py
+++ setup.py
@@ -5,8 +5,5 @@ from setuptools import setup, find_packages
 setup(
     name = 'esgf-compute-api',
     version = '1.0',
-    packages = find_packages('esgf'),
-    install_requires = [
-        'OWSLib>=0.11.2',
-    ],
+    packages = find_packages('esgf/'),
 )
