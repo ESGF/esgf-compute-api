e77e803aed7092806dc6d870949f8c697e2a435d
Jason Boutte <boutte.jason@gmail.com>
2016-11-29 17:43:43 +0000

    Removes setuptools requirement.

--- setup.py
+++ setup.py
@@ -2,15 +2,10 @@
 
 from setuptools import setup
 
-required = [
-    'owslib',
-]
-
 setup(
     name = 'esgf-compute-api',
     version = '1.0.0',
     packages = ['esgf'],
-    install_requires = required,
     author = 'Jason Boutte',
     author_email = 'boutte3@llnl.gov',
     description = 'ESGF Compute end-user API',
