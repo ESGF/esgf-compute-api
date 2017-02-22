429016a37b7cda6ccf9c95fb5da6350e44c64702
Jason Boutte <boutte.jason@gmail.com>
2017-02-22 21:24:59 +0000

    Adds various fixes to new xml library.

--- esgf/__init__.py
+++ esgf/__init__.py
@@ -1,29 +1,3 @@
 """
 WPS Client library imports.
 """
-from .mask import Mask
-
-from .gridder import Gridder
-
-from .operation import Operation
-from .process import Process
-
-from .named_parameter import NamedParameter
-
-from .variable import Variable
-
-from .domain import Domain
-
-from .dimension import Dimension
-
-from .parameter import Parameter
-
-from .wps import WPS
-from .wps import WPSRequestError
-from .wps import WPSResponseError
-from .wps import UnsupportedMethodError
-
-from .errors import WPSAPIError
-from .errors import WPSClientError
-from .errors import WPSServerError
-
