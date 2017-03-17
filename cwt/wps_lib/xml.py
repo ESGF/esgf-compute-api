84ab97933fbbdd15e47016c74336ffd9c28e8fce
Jason Boute <boutte.jason@gmail.com>
2017-03-17 23:31:59 +0000

    Adds documentation for wps_lib.xml module.

--- cwt/wps_lib/xml.py
+++ cwt/wps_lib/xml.py
@@ -13,6 +13,11 @@ logger = logging.getLogger(__name__)
 SUPPORTED_CONVERSION = (str, float, bool, int, long)
 
 class Translator(object):
+    """ Base translator class.
+
+    This class can be use to control how property names are transformed
+    to xml element/attribute names and vica-versa.
+    """
     def property_to_element(self, name):
         raise NotImplmenetedError()
 
@@ -26,6 +31,17 @@ class Translator(object):
         raise NotImplmenetedError()
 
 class Attribute(object):
+    """ Attribute decorator.
+
+    This decorator represents an xml attribute. If attach is not specified then
+    the attribute will be created on the root element.
+
+    Attributes:
+        namespace: A str namespace identifier.
+        value_type: A type that the attribute will hold. Default: str
+        required: A bool flag denoting whether the attribute is required. Default: False
+        attach: A str name of the element to set the attribute on.
+    """
     def __init__(self, **kwargs):
         self.namespace = kwargs.get('namespace')
         self.value_type = kwargs.get('value_type', str)
@@ -41,6 +57,66 @@ class Attribute(object):
         return ', '.join('%s=%s' % (x, y) for x, y in self.__dict__.iteritems())
 
 class Element(object):
+    """ Element decorator.
+
+    This decorator represents an xml element.
+
+    Examples of options.
+
+        @Element(namespace='ns')
+        def test(self): pass
+
+        <ns:test />
+
+        @Element(output_list=True)
+        def test(self): pass
+
+        <test>1</test>
+        <test>2</test>
+
+        @Element(child_tag='item', child_namespace='ns')
+        def test(self): pass
+
+        <test>
+            <ns:item>1</ns:item>
+        </test>
+
+        @Element(attr='value')
+        def test(self): pass
+
+        <test value="1" />
+
+        @Element(path='/hello/once/only', nsmap={'once': 'ns'})
+        def test(self): pass
+
+        <hello>
+            <ns:once>
+                <only>
+                    <test>1</test>
+                </only>
+            </ns:once>
+        </hello>
+
+        # NOTE: if store_value is present it will be the only property used
+        class Test(xml.XMLDocument):
+            @Element(store_value=True)
+            def test(self): pass
+
+        <Test>1</Test>
+
+    Attributes:
+        namespace: A str namesapce identifier.
+        output_list: A bool flag denoting wether the element has multiple children. Default: False
+        child_tag: A str value to create an element to wrap the value in.
+        child_namespace: A str namespace identifier for child_tag.
+        attr: A str value to name the attribute to hold the value.
+        path: A str path to nest the element under.
+        nsmap: A dict namespace map for the path.
+        value_type: A type that the element will hold. Default: str
+        store_value: A bool flag denoting that the elements contents will be the value.
+        minimum: An int setting the minimum number of items.
+        maximum: An int setting the maximum number of items.
+    """
     def __init__(self, **kwargs):
         self.namespace = kwargs.get('namespace')
         self.output_list = kwargs.get('output_list', False)
@@ -64,6 +140,12 @@ class Element(object):
         return ', '.join('%s=%s' % (x, y) for x, y in self.__dict__.iteritems())
 
 class XMLDocumentMarkupType(type):
+    """ XMLDocumentMarkupType.
+
+    This type must be used with XMLDocument class to create xml documents. 
+    This metaclass will collected the elements and attribute from properties
+    and attach them to the class.
+    """
     def __new__(mcs, name, bases, dct):
         def fget(key):
             def fget_wrapper(self):
@@ -97,6 +179,7 @@ class XMLDocumentMarkupType(type):
 
         cls = super(XMLDocumentMarkupType, mcs).__new__(mcs, name, bases, dct)
 
+        # Set default values for the properties
         for key, value in dct.iteritems():
             if hasattr(value, 'metadata'):
                 metadata = value.metadata
@@ -112,6 +195,7 @@ class XMLDocumentMarkupType(type):
 
         return cls
 
+# Error definitions
 class XMLParseError(Exception):
     pass
 
@@ -128,6 +212,39 @@ class ValidationError(Exception):
     pass
 
 class XMLDocument(object):
+    """ XMLDocument class.
+        
+    This class must be inherited by as subclass. This works in conjunction with
+    XMLDocumentMarkupType. Element and attribute decorators can be added to 
+    method definitions to create a pseudo XML schema.
+
+    class Car(XMLDocument):
+        __metaclass__ = XMLDocumentMarkupType
+
+        def __init__(self, **kwargs):
+            super(Car, self).__init__(**kwargs)
+
+        @Attribute()
+        def color(self): pass
+
+        @Element()
+        def engine(self): pass
+
+    c = Car(color='blue', engine='V8')
+
+    print c.xml()
+
+    <Car color="blue">
+        <engine>V8</engine>
+    </Car>
+
+    Attributes:
+        namespace: A str namespace identifier for the root element.
+        nsmap: A dict mapping namespace identifiers to namespace urls.
+        tag: A str value to be substituted for the class name as the root element name.
+        translatpor: A Translator to be use in converting names.
+        **kwargs: A dict of default property values.
+    """
     def __init__(self, namespace=None, nsmap=None, tag=None, translator=None, **kwargs):
         self.namespace = namespace
         self.nsmap = nsmap
@@ -159,6 +276,13 @@ class XMLDocument(object):
         return doc
 
     def validate(self):
+        """ Validates the property values against their definitions.
+
+        Elements bounds are validated as well as attribute requirements.
+
+        Raise:
+            ValidationError: An error occurred validating one of the properties.
+        """
         for name, metadata in self.attributes.iteritems():
             if metadata.required:
                 value = getattr(self, name)
@@ -168,9 +292,11 @@ class XMLDocument(object):
                             (self.__class__.__name__, name,))
 
         for name, metadata in self.elements.iteritems():
+            # Check property that stores a list
             if metadata.output_list:
                 value = getattr(self, name)
 
+                # Check the minimum
                 if metadata.minimum > 0:
                     if not isinstance(value, (list, tuple)):
                         raise ValidationError('%s Element %s was expecting a '
@@ -179,12 +305,14 @@ class XMLDocument(object):
                         raise ValidationError('%s Element %s requires a minimum '
                                 'of %s values' % (self.__class__.__name__, name, metadata.minimum))
 
+                # Check the maximum
                 if metadata.maximum is not None:
                     if len(value) > metadata.maximum:
                         raise ValidationError('%s Element %s requires max %s'
                                 ' values, %s were provided' %
                                 (self.__class__.__name__, name, metadata.maximum, len(value)))
             else:
+                # Check if a single value is required
                 if metadata.minimum > 0:
                     value = getattr(self, name)
 
@@ -196,11 +324,20 @@ class XMLDocument(object):
                                 'single value' % (self.__class__.__name__, name,))
 
     def __append_value(self, name, node, metadata):
+        """ Appends a value to a property that stores a list.
+        
+        Args:
+            name: A str name of the property.
+            node: A etree.Element whose value will be converted and stored.
+            metadata: A Element/Attribute of the property being stored.
+        """
+        # Check for an existing list or create a new one
         values = getattr(self, name)
 
         if values is None:
             values = []
 
+        # Convert etree.Element to XMLDocument
         if issubclass(metadata.value_type, XMLDocument):
             value = metadata.value_type.from_element(node, self.translator)
         else:
@@ -215,11 +352,21 @@ class XMLDocument(object):
         setattr(self, name, values)
 
     def __store_value(self, raw_name, name, node, metadata):
+        """ Stores a node in a property.
+
+        Args:
+            raw_name: A str unformatted name for the node.
+            name: A str formatted name for the node.
+            node: A etree.Element whose value will be store.
+            metadata: An Element/Attribute associatd with the property.
+        """
         logger.debug('Storing value "%s", "%s", "%s"', name, node.tag, metadata)
 
+        # Process a list or single value
         if metadata.output_list:
             self.__append_value(name, node, metadata)
         else:
+            # Check if a variable typed value is being stored.
             if isinstance(metadata.value_type, (list, tuple)):
                 try:
                     target = [x for x in metadata.value_type if x.__name__ == raw_name][0]
@@ -235,6 +382,14 @@ class XMLDocument(object):
             setattr(self, name, value)
 
     def __match_parent(self, node):
+        """ Processes up the XML tree to find a parent who is a known property.
+
+        Args:
+            node: A etree.Element that is the starting node.
+
+        Return:
+            A tuple containing the etree.Element and Element of the parent.
+        """
         parent = node
         parent_element = None
         parent_metadata = None
@@ -259,6 +414,13 @@ class XMLDocument(object):
         return parent_element, parent_metadata
 
     def __set_property(self, name, value, metadata):
+        """ Converts and stores a value in a property.
+
+        Args:
+            name: A str name of the property.
+            value: A str value to be stored.
+            metadata: An Element/Attribute that belongs to the property.
+        """
         try:
             value = metadata.value_type(value)
         except ValueError:
@@ -268,6 +430,14 @@ class XMLDocument(object):
         setattr(self, name, value)
 
     def __match_element_value_type(self, tag):
+        """ Find element by its value_type.
+
+        Args:
+            tag: A str name of the element name.
+
+        Return:
+            A tuple including the name of the property and its related metadata.
+        """
         for name, metadata in self.elements.iteritems():
             candidates = metadata.value_type
 
@@ -281,8 +451,15 @@ class XMLDocument(object):
         return None, None
 
     def parse_xml(self, root):
+        """ Parses an XML document according to the class definition.
+
+        Args:
+            root: An etree.Element being the root of the document.
+        """
+        # Remove the namespace
         cls_name = re.sub('^{.*}', '', root.tag)
 
+        # Validate that we're parsing the correct class
         if not (cls_name == self.__class__.__name__ or
                 (self.tag is not None and cls_name == self.tag)):
             raise ValidationError('XML does not match class definition')
@@ -292,6 +469,7 @@ class XMLDocument(object):
         logger.debug('Translator %s', self.translator)
         logger.debug('Known elements %s', self.elements.keys())
 
+        # Process all the root attributes
         for name, value in root.attrib.iteritems():
             name = re.sub('^{.*}', '', name)
 
@@ -303,6 +481,7 @@ class XMLDocument(object):
 
                 self.__set_property(name, value, metadata)
 
+        # Process the XML tree using depth first search
         stack = [root]
 
         while len(stack):
@@ -312,6 +491,7 @@ class XMLDocument(object):
 
             name = re.sub('^{.*}', '', tag)
                 
+            # Translate the name if needed
             if self.translator is not None:
                 trans_name = self.translator.element_to_property(name)
             else:
@@ -319,7 +499,7 @@ class XMLDocument(object):
             
             logger.debug('Processing element "%s" (%s)', trans_name, name)
 
-            # Check each non-root element for attached attributes
+            # On non-root nodes check for attributes and store their values.
             if node != root:
                 for atrans_name, value in node.attrib.iteritems():
                     if self.translator is not None:
@@ -330,17 +510,20 @@ class XMLDocument(object):
 
                         self.__set_property(atrans_name, value, metadata)
 
+            # Check if the current node is defined in the class
             if trans_name in self.elements:
                 metadata = self.elements[trans_name]
 
                 logger.debug('Element "%s" is a known property %s', trans_name, metadata)
 
+                # Process a property value that is stored as an attribute.
                 if metadata.attr is not None:
                     if metadata.attr in node.attrib:
                         value = node.attrib[metadata.attr]
 
                         self.__set_property(node.tag, value, metadata)
                 elif (metadata.child_tag is not None or
+                    # Process child tags or multiple child tags
                         (metadata.output_list and
                             issubclass(metadata.value_type, XMLDocument) and
                             metadata.path is not None) or
@@ -350,21 +533,28 @@ class XMLDocument(object):
                 else:
                     self.__store_value(name, trans_name, node, metadata)
             else:
+                # Node is not a known property we try to process it according
+                # to the rules or we just add it's children to the stack.
                 logger.debug('Handling unknown element "%s"', name)
 
                 match_trans_name, match_metadata = self.__match_element_value_type(name)
 
                 logger.debug('Match element by value_type "%s": "%s"', match_trans_name, match_metadata)
 
+                # Search for a defined element whos value_type matches the nodes
+                # name. This is a case where value_type may accept multiple types.
                 if match_trans_name is not None:
                     self.__store_value(name, match_trans_name, node, match_metadata)
                 else:
                     handled = False
 
+                    # Check to see if the an Element with store_value has been
+                    # defined, and the value of the node should be stored.
                     for ename, emeta in self.elements.iteritems():
                         if emeta.store_value:
                             children = node.getchildren()
 
+                            # Special case where the stored value is XML
                             if len(children) > 0:
                                 value = '\n'.join(etree.tostring(x, pretty_print=True) for x in children)
 
@@ -377,17 +567,36 @@ class XMLDocument(object):
                             break
 
                     if not handled:
+                        # Last resort, run up the tree searching for a parent
+                        # who can store the value
                         parent_element, parent_metadata = self.__match_parent(node)
 
                         if parent_element is not None:
                             self.__store_value(name, parent_element.tag, node, parent_metadata)
                         else:
+                            # End of the line just append the child nodes to the
+                            # stack
                             for c in node.getchildren():
                                 stack.append(c)
 
         logger.debug('%s END PARSING "%s" %s', '#'*6, re.sub('^{.*}', '', root.tag), '#'*6)
 
     def __generate_name(self, name, namespace, metadata=None):
+        """ Generate an Element/Attribute name.
+
+        If a namespace is provided make sure it is present in the classes nsmap.
+
+        Args:
+            name: A str base name.
+            namespace: A str namespace identifier.
+            metadata: A Element/Attribute associated to the property
+
+        Return:
+             A str formatted Element/Attribute name
+
+        Raises:
+            MissingNamespaceError: The namespace is not present in the classes nsmap.
+        """
         new_name = name
 
         if metadata is not None and self.translator is not None:
@@ -408,6 +617,18 @@ class XMLDocument(object):
                     (namespace,))
 
     def __generate_element(self, parent, name, value, metadata, cache):
+        """ Creates a new element.
+        
+        Args:
+            parent: An etree.Element that will be the parent of the new node.
+            name: A str name of the new node.
+            value: An object whos value will be stored.
+            metadata: An Element/Attribute associated with the property.
+            cache: A dict that will contain all the new nodes.
+
+        Return:
+            A new etree.Element.
+        """
         if value is None:
             return None
 
@@ -431,8 +652,16 @@ class XMLDocument(object):
             cache[name] = new_element
 
     def generate_xml(self):
+        """ Generate XML from class definition.
+
+        Process the classes elements and attributes that have been declard to
+        create an XML document.
+
+        """
+        # Validate all the document constraints
         self.validate()
 
+        # Generate our root nodes name
         cls_name = self.__class__.__name__
 
         if self.tag is not None:
@@ -444,9 +673,11 @@ class XMLDocument(object):
 
         cache = {}
 
+        # Process all of the declared elements
         for name, metadata in self.elements.iteritems():
             parent = root
 
+            # Contents of the root node will be only this value
             if metadata.store_value:
                 value = getattr(self, name)
 
@@ -454,6 +685,7 @@ class XMLDocument(object):
 
                 break
 
+            # Create a path if declared
             if metadata.path is not None:
                 for s in metadata.path.split('/'):
                     if s != '':
@@ -497,6 +729,8 @@ class XMLDocument(object):
 
                 self.__generate_element(parent, name, value, metadata, cache)
 
+        # Process the attributes last since they may be attached to nodes
+        # created by Elements that declared path values.
         for name, metadata in self.attributes.iteritems():
             value = getattr(self, name)
 
@@ -514,4 +748,5 @@ class XMLDocument(object):
         return root
 
     def xml(self, pretty_print=False):
+        """ Generate str XML document from etree.Element. """
         return etree.tostring(self.generate_xml(), pretty_print=True)
