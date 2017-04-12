855c3f2204e48ec40ba40746c8260666decc2807
Jason Boutte <boutte.jason@gmail.com>
2017-04-12 22:39:25 +0000

    Fixes parameter unittest.

--- cwt/test/test_parameter.py
+++ cwt/test/test_parameter.py
@@ -2,49 +2,36 @@
 Unittest for parameter class.
 """
 
-from unittest import TestCase
+import unittest
 
-from cwt import Parameter
+import cwt
 
-class TestImpl(Parameter):
+class HelloWorld(cwt.Parameter):
     """ Test class for overriding method. """
     def __init__(self):
         """ Testimpl init. """
-        super(TestImpl, self).__init__('test')
+        super(HelloWorld, self).__init__('test')
 
     def parameterize(self):
         """ Overridden parameterize method. """
         return 'param'
 
-class TestParameter(TestCase):
+class TestParameter(unittest.TestCase):
     """ Test Case for Parameter class. """
-
-    def test_repr(self):
-        """ Test repr value. """
-        param = Parameter('v0')
-
-        self.assertEqual(repr(param),
-                         "Parameter(name='v0')")
-
-    def test_str(self):
-        """ Test str value. """
-        param = Parameter('v0')
-
-        self.assertEqual(str(param),
-                         "name=v0")
 
     def test_from_dict(self):
         """ Test from dict. """
         with self.assertRaises(NotImplementedError):
-            param = Parameter.from_dict('test')
+            param = cwt.Parameter.from_dict('test')
 
     def test_parameterize(self):
         """ Testing overriding parameterize method. """
 
         with self.assertRaises(NotImplementedError):
-            param = Parameter('test')
+            param = cwt.Parameter('test')
+
             param.parameterize()
 
-        over_param = TestImpl()
+        over_param = HelloWorld()
 
         self.assertEqual(over_param.parameterize(), 'param')
