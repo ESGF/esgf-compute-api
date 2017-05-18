651671ea0839b1b1e21082bb2fedbab73c1a71aa
Jason Boutte <boutte.jason@gmail.com>
2017-05-18 00:26:49 +0000

    Adds some predefined dimensions.

--- cwt/__init__.py
+++ cwt/__init__.py
@@ -15,11 +15,17 @@ from .process import *
 from .variable import *
 from .wps import *
 
+NorthernHemisphere = Dimension('latitude', 0, 90)
+SouthernHemisphere = Dimension('latitude', -90, 0)
+Tropics = Dimension('latitude', -23.4, 23.4)
+ArticZone = Dimesnion('latitude', 66.6, 90.0)
+AntarcticZone = Dimension('latitude', -90, -66.6)
+
 T21 = Gridder(grid='gaussian~32')
 T42 = Gridder(grid='gaussian~64')
 T63 = Gridder(grid='gaussian~96')
 
-UQ = Gridder(grid='uniform~.25x.25')
-UH = Gridder(grid='uniform~.5x.5')
+UQuart = Gridder(grid='uniform~.25x.25')
+UHalf = Gridder(grid='uniform~.5x.5')
 U1 = Gridder(grid='uniform~1x1')
 U2 = Gridder(grid='uniform~2x2')
