07d4112a6db83c44b4e63d96f934ce422c1c4d07
ThomasMaxwell <thomas.maxwell@nasa.gov>
2017-08-21 16:07:17 -0400

    Add test script

---
+++ cwt/test/test_workflow.py
@@ -0,0 +1,22 @@
+import cwt, os
+
+# datainputs = """[domain=[{"name":"d0","lat":{"start":70,"end":90,"system":"values"},"lon":{"start":5,"end":45,"system":"values"}}],
+# variable=[{"uri":"file:///dass/nobackup/tpmaxwel/.edas/cache/collections/NCML/MERRA_TAS1hr.ncml","name":"tas:v1","domain":"d0"}],
+# operation=[{"name":"CDSpark.average","input":"v1","domain":"d0","axes":"xy"}]]"""
+
+class TestWorkflow:
+
+    def run( self ):
+        domain_data = { 'id': 'd0', 'lat': {'start':70, 'end':90, 'crs':'values'}, 'lon': {'start':5, 'end':45, 'crs':'values'} }
+        d0 = cwt.Domain.from_dict(domain_data)
+
+        op = cwt.Operation.from_dict( { 'name': "CDSpark.average" } )
+        op.add_input( cwt.Variable("file:///dass/nobackup/tpmaxwel/.edas/cache/collections/NCML/MERRA_TAS1hr.ncml", "tas", domain="d0", axes="xy" ) )
+
+        wps = cwt.WPS( 'http://localhost:5327/wps', log=True, log_file=os.path.expanduser("~/esgf_api.log") );  """:type : cwt.WPS """
+        wps.init()
+        process = cwt.Process( wps, op );                    """:type : cwt.Process """
+        process.execute( None, d0, [], True, True, "GET" )
+
+executor = TestWorkflow()
+executor.run()
