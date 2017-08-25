a29d83866142332205e89cdddb4e0c77bc6e9652
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2017-08-25 17:32:01 -0400

    Update test

--- cwt/test/test_capabilities.py
+++ cwt/test/test_capabilities.py
@@ -5,8 +5,6 @@ class DemoWorkflow:
     def run( self ):
 
         wps = cwt.WPS( 'http://localhost:9000/wps', log=True, log_file=os.path.expanduser("~/esgf_api.log") )
-        wps.init()
-
         capabilities = wps.capabilities
 
         print "Capabilities: " + capabilities
