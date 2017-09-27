5ec2e653ce4deaba17a6be125b2df26f9769ca10
ThomasMaxwell <thomas.maxwell@nasa.gov>
2017-09-27 14:15:45 -0400

    Update test

--- cwt/plotters.py
+++ cwt/plotters.py
@@ -2,6 +2,9 @@ import os, time
 import logging, cdms2, vcs
 import cdms2, datetime, matplotlib, urllib3
 import matplotlib.pyplot as plt
+import plotly.offline as py
+import plotly.graph_objs as go
+import pandas as pd
 
 
 class PlotMgr:
@@ -9,14 +12,25 @@ class PlotMgr:
     def __init__(self):
         self.logger = logging.getLogger('cwt.wps')
 
-    def mpl_timeplot( self, dataPath, varName ):
+    def mpl_timeplot( self, dataPath, varName="Nd4jMaskedTensor" ):
         self.logger.info( "Plotting file: " +  dataPath )
         f = cdms2.openDataset(dataPath)
-        var = f( varName ) # , time=slice(0,1),level=slice(10,11) )
-        rank = len(var.shape)
-        timeSeries = var[:,0,0] if( rank == 3 ) else var[:,0,0,0] if( rank == 4 ) else var[:,0] if( rank == 2 ) else var[:]
-        list_of_datetimes = [datetime.datetime(x.year, x.month, x.day, x.hour, x.minute, int(x.second)) for x in var.getTime().asComponentTime()]
+        timeSeries = f( varName, squeeze=1 ) # , time=slice(0,1),level=slice(10,11) )
+        list_of_datetimes = [datetime.datetime(x.year, x.month, x.day, x.hour, x.minute, int(x.second)) for x in timeSeries.getTime().asComponentTime()]
         dates = matplotlib.dates.date2num(list_of_datetimes)
         plt.plot_date(dates, timeSeries.data )
         plt.gcf().autofmt_xdate()
         plt.show()
+
+    def plotly_timeplot( self, dataPath, varName="Nd4jMaskedTensor" ):
+        self.logger.info( "Plotting file: " +  dataPath )
+        f = cdms2.openDataset(dataPath)
+        timeSeries = f( varName, squeeze=1 )
+        datetimes = pd.to_datetime(timeSeries.getTime().asdatetime())
+        data = [go.Scatter(x=datetimes, y=timeSeries)]
+        print(py.plot(data, output_type='file', filename='testTimeSeries.html', auto_open=False))
+
+
+
+
+
--- cwt/test/test_workflow.py
+++ cwt/test/test_workflow.py
@@ -6,12 +6,15 @@ import cwt, os
 # import matplotlib.pyplot as plt
 
 # host = 'https://www-proxy-dev.nccs.nasa.gov/edas/wps/cwt'
-host ="https://dptomcat03-int/wps/cwt"
+
 
 class TestWorkflow:
+    plotter = cwt.initialize()
+    host ="https://dptomcat03-int/wps/cwt"
+    wps = cwt.WPS( host, log=True, log_file=os.path.expanduser("~/esgf_api.log"), verify=False )
 
     def run( self ):
-        plotter = cwt.initialize()
+
 
         domain_data = { 'id': 'd0', 'lat': {'start':70, 'end':90, 'crs':'values'}, 'lon': {'start':5, 'end':45, 'crs':'values'}, 'time': {'start':0, 'end':1000, 'crs':'indices'} }
         d0 = cwt.Domain.from_dict(domain_data)
@@ -22,11 +25,16 @@ class TestWorkflow:
         op =  cwt.Process.from_dict( op_data ) # """:type : Process """
         op.set_inputs( inputs )
 
-        wps = cwt.WPS( host, log=True, log_file=os.path.expanduser("~/esgf_api.log"), verify=False )
-        wps.execute( op, domain=d0, async=True )
+        self.wps.execute( op, domain=d0, async=True )
 
-        dataPath = wps.download_result(op)
-        plotter.mpl_timeplot(dataPath,"Nd4jMaskedTensor")
+        dataPath = self.wps.download_result(op)
+        self.plotter.plotly_timeplot(dataPath)
+
+    def test_plot(self):
+        self.plotter.plotly_timeplot("/tmp/testData.nc")
+
+    def testCapabilities(self):
+        print self.wps.capabilities
 
 executor = TestWorkflow()
-executor.run()
+executor.testCapabilities()
