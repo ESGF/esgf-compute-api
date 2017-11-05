ec128ec141debe212b72c92a1ab72e0c2fc87e86
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2017-11-05 14:15:32 -0500

    Fix ensemble test

--- cwt/test/test_ensemble_ave.py
+++ cwt/test/test_ensemble_ave.py
@@ -1,11 +1,12 @@
-import cwt
+import cwt, os
 
 def getEnsembleInput( collection, domain ):
     variable = cwt.Variable( "collection://" + collection, "ta", domain=domain, name=collection )
     return variable
 
 plotter = cwt.initialize()
-wps = cwt.WPS( "https://dptomcat03-int/wps/cwt" )
+host = "https://dptomcat03-int/wps/cwt"
+wps = cwt.WPS(host, log=True, log_file=os.path.expanduser("~/esgf_api.log"), verify=False)
 
 d0 = cwt.Domain.from_dict( { 'id': 'd0',
       'time': {'start':'1980-01-01T00:00:00','end':'1980-01-01T23:59:00','crs':'timestamps'},
