b99ca3b78c7069f28e3b03d619d0804cc80ee93e
ThomasMaxwell <thomas.maxwell@nasa.gov>
2017-11-07 18:29:03 -0500

    Update tests

--- cwt/plotters.py
+++ cwt/plotters.py
@@ -9,7 +9,7 @@ class PlotMgr:
     def __init__(self):
         self.logger = logging.getLogger('cwt.wps')
 
-    def mpl_timeplot( self, dataPath, varName="Nd4jMaskedTensor" ):
+    def mpl_timeplot( self, dataPath, varName="Nd4jMaskedTensor", show_points=False ):
         if dataPath:
             self.logger.info( "Plotting file: " +  dataPath )
             f = cdms2.openDataset(dataPath)
@@ -17,7 +17,8 @@ class PlotMgr:
             datetimes = [datetime.datetime(x.year, x.month, x.day, x.hour, x.minute, int(x.second)) for x in timeSeries.getTime().asComponentTime()]
             dates = matplotlib.dates.date2num(datetimes)
             fig, ax = plt.subplots()
-            ax.plot(dates, timeSeries.data, 'bo-' )
+            if( show_points): ax.plot(dates, timeSeries.data, 'bo-' )
+            else: ax.plot( dates, timeSeries.data )
             ax.xaxis.set_major_formatter( mdates.DateFormatter('%b %Y') )
             ax.grid(True)
             fig.autofmt_xdate()
