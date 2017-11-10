ce57569523d5b6cee41d532df1ec315c708b8c08
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2017-11-10 11:37:46 -0500

    Update tests

---
+++ cwt/test/local_tave_comparisons.py
@@ -0,0 +1,44 @@
+import cwt, os, time
+
+def create_tempdir():
+    temp_dir = os.path.expanduser( "~/.edas" )
+    try: os.makedirs( temp_dir, 0755 )
+    except Exception: pass
+    return temp_dir
+
+class TestWorkflow:
+    plotter = cwt.initialize()
+    host ="https://127.0.0.1:9999/cwt"
+    wps = cwt.WPS( host, log=True, log_file=os.path.expanduser("~/esgf_api.log"), verify=False )
+    temp_dir = create_tempdir()
+
+    def sia_comparison_time_ave( self, start_year, end_year ):
+
+        domain_data = { 'id': 'd0','time': {'start':str(start_year)+'-01-01T00:00:00','end':str(end_year)+'-12-31T23:00:00','crs':'timestamps'  } }
+        d0 = cwt.Domain.from_dict(domain_data)
+
+        print "\nExecuing global time average for variabe 'tas' from collection 'iap-ua_eraint_tas1hr' for " + str(end_year-start_year+1) + " years, starting with " + str(start_year) +"\n"
+
+        inputs = cwt.Variable( "collection://iap-ua_eraint_tas1hr", "tas", domain="d0" )
+
+        op_data =  { 'name': "CDSpark.ave", 'axes': "t" }
+        op =  cwt.Process.from_dict( op_data )
+        op.set_inputs( inputs )
+
+        op.set_inputs()
+
+        start = time.time()
+        self.wps.execute( op, domains=[d0], async=True )
+
+        dataPath = self.wps.download_result( op, self.temp_dir )
+        end = time.time()
+        print "\nCompleted execution in " + str(end-start) + " secs\n"
+        self.plotter.mpl_spaceplot(dataPath)
+
+executor = TestWorkflow()
+
+# Edit the start and end years here:
+
+executor.sia_comparison_time_ave(1958,1962)   #  Holdings:  1958 - 2001
+
+
