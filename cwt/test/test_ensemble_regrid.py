8de777e8f4355fb617c1cbd304855e7724d20a92
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2017-11-19 13:37:05 -0500

    Add regrid test

---
+++ cwt/test/test_ensemble_regrid.py
@@ -0,0 +1,10 @@
+import cwt, os
+collections =  [ "cip_jra55_6hr", "cip_merra2_6hr", "cip_cfsr_6hr", "cip_eraint_6hr" ]
+d0 = cwt.Domain.from_dict( { 'id': 'd0', 'time': {'start':'1980-01-01T00:00:00','end':'1980-12-31T23:00:00','crs':'timestamps'}, 'lev': {'start':'80000','end':'80000','crs':'values'} } )
+
+plotter = cwt.initialize()
+wps = cwt.WPS( "https://edas.nccs.nasa.gov/wps/cwt" )
+ens_inputs = [ cwt.Variable( "collection://" + collection, "ta", domain=d0, name=collection ) for collection in collections ]
+ens_ave = cwt.Process.from_dict({ 'name': "CDSpark.eAve", "cwt": "~cip_jra55_6hr", "input": ens_inputs })
+wps.execute( ens_ave, domains=[d0] )
+wps.get_status( ens_ave )
