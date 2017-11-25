9e0b974dc85af1cdefd649a037b2c352180be41b
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2017-11-25 13:08:00 -0500

    Update tests

--- cwt/test/reanalysis_ensemble_ave.py
+++ cwt/test/reanalysis_ensemble_ave.py
@@ -1,7 +1,7 @@
 import cwt, getpass
 levels = """100000,97500,95000,92500,90000,87500,85000,82500,80000,77500,75000,70000,65000,60000,55000,50000,45000,40000,35000,30000,25000,20000,15000,10000"""
 collections =  [ "cip_jra55_6hr", "cip_merra2_6hr", "cip_cfsr_6hr", "cip_eraint_6hr" ]
-d0 = cwt.Domain.from_dict( { 'id': 'd0', 'time': {'start':'1980-01-01T00:00:00','end':'1980-12-31T23:00:00','crs':'timestamps'} } )
+d0 = cwt.Domain.from_dict( { 'id': 'd0', 'time': {'start':'1980-01-01T00:00:00','end':'1980-01-31T23:59:00','crs':'timestamps'} } )
 
 def getEnsembleInput( collection, levels, domain ):
     variable = cwt.Variable( "collection://" + collection, "ta", domain=domain, name="ta-" + collection )
