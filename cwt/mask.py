dfc5108359a289bbc01310a538faa4ed06f936dc
ThomasMaxwell <thomas.maxwell@nasa.gov>
2017-11-28 11:56:19 -0500

    simplify uuid

--- cwt/mask.py
+++ cwt/mask.py
@@ -43,7 +43,7 @@ class Mask(object):
         self.operation = operation
 
         if not name:
-            name = str(uuid()).split("-")[-1]
+            name = str(uuid())[26:]
 
         self.name = name
 
