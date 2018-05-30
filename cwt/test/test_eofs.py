f751b8cfe6b95a309e8ce9f30d7f839fcfcef5d5
ThomasMaxwell <thomas.maxwell@nasa.gov>
2018-05-30 14:39:37 -0400

    Add eofs example

--- cwt/test/test_eofs.py
+++ cwt/test/test_eofs.py
@@ -1,49 +1,83 @@
-import cartopy.crs as ccrs
-import cartopy.feature as cfeature
-from netCDF4 import Dataset
-import matplotlib.pyplot as plt
-import numpy as np
-from eofs.standard import Eof
+import cdms2 as cdms
+import cdutil
+import cdtime
+from eofs.cdms import Eof
+import vcs
+import string
 
-# inFile = '/Users/tpmaxwel/Dropbox/Tom/Data/CIP/MERRA2/mon/tas/tas.1980-2015.nc'
-# filename = '/dass/pubrepo/CREATE-IP/data/reanalysis/NOAA-NCEP/CFSR/mon/atmos/clt/clt_Amon_reanalysis_CFSR_197901-201712.nc'
-filename = '/dass/adm/edas/eofs-1.3.0/lib/eofs/examples/example_data/sst_ndjfm_anom.nc'
-ncin = Dataset(filename, 'r')
-sst = ncin.variables['sst'][:]
-lons = ncin.variables['longitude'][:]
-lats = ncin.variables['latitude'][:]
-ncin.close()
-print "Completed data read"
+#===========================================================================================================
+# DATA
+#-----------------------------------------------------------------------------------------------------------
+# Open file ---
+# data_path = '/clim_obs/obs/ocn/mo/tos/UKMETOFFICE-HadISST-v1-1/130122_HadISST_sst.nc' ## Put your file here
+data_path = '/dass/pubrepo/CREATE-IP/data/reanalysis/NOAA-NCEP/CFSR/mon/atmos/tas/tas_Amon_reanalysis_CFSR_197901-201712.nc'
+f = cdms.open(data_path)
 
-coslat = np.cos(np.deg2rad(lats))
-wgts = np.sqrt(coslat)[..., np.newaxis]
-print "Completed weights generation"
+# Set time period ---
+start_year = 1980
+end_year = 2000
+start_time = cdtime.comptime(start_year)
+end_time = cdtime.comptime(end_year)
 
-solver = Eof(sst, weights=wgts)
-print "Created Solver"
+# Load variable ---
+d = f('tas',time=(start_time,end_time),longitude=(0,360),latitude=(-90,90)) # Provide proper variable name
 
-eof1 = solver.eofsAsCorrelation(neofs=1)
-pc1 = solver.pcs(npcs=1, pcscaling=1)
-print "Computed pc1"
+# Reomove annual cycle ---
+d_anom = cdutil.ANNUALCYCLE.departures(d)
 
-# Plot the leading EOF expressed as correlation in the Pacific domain.
-clevs = np.linspace(-1, 1, 11)
-ax = plt.axes(projection=ccrs.PlateCarree(central_longitude=190))
-fill = ax.contourf(lons, lats, eof1.squeeze(), clevs, transform=ccrs.PlateCarree(), cmap=plt.cm.RdBu_r)
-ax.add_feature(cfeature.LAND, facecolor='w', edgecolor='k')
-cb = plt.colorbar(fill, orientation='horizontal')
-cb.set_label('correlation coefficient', fontsize=12)
-plt.title('EOF1 expressed as correlation', fontsize=16)
-print "4"
-# Plot the leading PC time series.
-plt.figure()
-years = range(1979, 2017)
-plt.plot(years, pc1, color='b', linewidth=2)
-plt.axhline(0, color='k')
-plt.title('PC1 Time Series')
-plt.xlabel('Year')
-plt.ylabel('Normalized Units')
-plt.xlim(1979, 2017)
-plt.ylim(-3, 3)
+# EOF (take only first variance mode...) ---
+solver = Eof(d_anom, weights='area')
+eof = solver.eofsAsCovariance(neofs=1)
+pc = solver.pcs(npcs=1, pcscaling=1) # pcscaling=1: scaled to unit variance
+                                     # (divided by the square-root of their eigenvalue)
+frac = solver.varianceFraction()
 
-plt.show()
+# Sign control if needed ---
+eof = eof * -1
+pc = pc * -1
+
+#===========================================================================================================
+# Plot
+#-----------------------------------------------------------------------------------------------------------
+# Create canvas ---
+canvas = vcs.init(geometry=(900,800))
+
+canvas.open()
+template = canvas.createtemplate()
+
+# Turn off no-needed information -- prevent overlap
+template.blank(['title','mean','min','max','dataname','crdate','crtime',
+      'units','zvalue','tvalue','xunits','yunits','xname','yname'])
+
+# Color setup ---
+canvas.setcolormap('bl_to_darkred')
+iso = canvas.createisofill()
+iso.levels = [-1, -0.9, -0.8, -0.7, -0.6, -0.5, -0.4, -0.3, -0.2, -0.1, 0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+iso.ext_1 = 'y' # control colorbar edge (arrow extention on/off)
+iso.ext_2 = 'y' # control colorbar edge (arrow extention on/off)
+cols = vcs.getcolors(iso.levels)
+iso.fillareacolors = cols
+iso.missing = 0 # Set missing value color as same as background
+
+# Map projection ---
+p = vcs.createprojection()
+p.type = 'robinson'
+iso.projection = p
+
+# Plot ---
+canvas.plot(eof[0],iso,template)
+
+# Title ---
+plot_title = vcs.createtext()
+plot_title.x = .5
+plot_title.y = .97
+plot_title.height = 23
+plot_title.halign = 'center'
+plot_title.valign = 'top'
+plot_title.color='black'
+percentage = str(round(float(frac[0]*100.),1)) + '%' # % with one floating number
+plot_title.string = 'EOF first mode, HadISST('+str(start_year)+'-'+str(end_year)+'), '+percentage
+canvas.plot(plot_title)
+
+# Save output as image file ---
+canvas.png('eof_analysis.png')
