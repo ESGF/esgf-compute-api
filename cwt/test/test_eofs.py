b74c6ec3ad1026e4f2c241d78c013f6475160a1a
ThomasMaxwell <thomas.maxwell@nasa.gov>
2018-05-31 18:00:57 -0400

    Update eofs test

--- cwt/test/test_eofs.py
+++ cwt/test/test_eofs.py
@@ -17,6 +17,8 @@ f = cdms.open(data_path)
 
 start_year = 1980
 end_year = 2000
+nModes = 4
+
 start_time = cdtime.comptime(start_year)
 end_time = cdtime.comptime(end_year)
 
@@ -44,30 +46,33 @@ canvas.open()
 template = canvas.createtemplate()
 template.blank(['title','mean','min','max','dataname','crdate','crtime','units','zvalue','tvalue','xunits','yunits','xname','yname'])
 canvas.setcolormap('bl_to_darkred')
-iso = canvas.createisofill()
-level_distribution = np.array( [-1, -0.9, -0.8, -0.7, -0.6, -0.5, -0.4, -0.3, -0.2, -0.1, 0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0] )
-iso.levels = list( level_distribution*0.01 )
-iso.ext_1 = 'y' # control colorbar edge (arrow extention on/off)
-iso.ext_2 = 'y' # control colorbar edge (arrow extention on/off)
-cols = vcs.getcolors(iso.levels, range(16,240), split=0)
-iso.fillareacolors = cols
-iso.missing = 0
 
-p = vcs.createprojection()
-p.type = 'robinson'
-iso.projection = p
+for iPlot in range(nModes):
+    iso = canvas.createisofill()
+    level_distribution = np.array( [-1.6, -1.3, -1.0, -0.7, -0.6, -0.5, -0.4, -0.3, -0.2, -0.1, 0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 1.0, 1.3, 1.6] )
+    iso.levels = list( level_distribution*0.01 )
+    iso.ext_1 = 'y' # control colorbar edge (arrow extention on/off)
+    iso.ext_2 = 'y' # control colorbar edge (arrow extention on/off)
+    cols = vcs.getcolors(iso.levels, range(16,240), split=0)
+    iso.fillareacolors = cols
+    iso.missing = 0
 
-canvas.plot(eof[0],iso,template)
+    p = vcs.createprojection()
+    p.type = 'robinson'
+    iso.projection = p
 
-plot_title = vcs.createtext()
-plot_title.x = .5
-plot_title.y = .97
-plot_title.height = 23
-plot_title.halign = 'center'
-plot_title.valign = 'top'
-plot_title.color='black'
-percentage = str(round(float(frac[0]*100.),1)) + '%' # % with one floating number
-plot_title.string = 'EOF first mode, MERRA2 TS('+str(start_year)+'-'+str(end_year)+'), '+percentage
-canvas.plot(plot_title)
-canvas.png('eof_analysis.png')
+    canvas.plot(eof[iPlot],iso,template)
+
+    plot_title = vcs.createtext()
+    plot_title.x = .5
+    plot_title.y = .97
+    plot_title.height = 23
+    plot_title.halign = 'center'
+    plot_title.valign = 'top'
+    plot_title.color='black'
+    percentage = str(round(float(frac[iPlot]*100.),1)) + '%'
+    plot_title.string = 'EOF mode ' + str(iPlot) + ', MERRA2 TS('+str(start_year)+'-'+str(end_year)+'), '+percentage
+    canvas.plot(plot_title)
+    canvas.png('eof_analysis-{0}.png'.format(iPlot))
+    canvas.clear()
 
