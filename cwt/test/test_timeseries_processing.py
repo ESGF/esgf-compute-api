2a766d0a61be7fa20af56d53a5bae986d495e3ad
ThomasMaxwell <thomas.maxwell@nasa.gov>
2018-06-04 15:37:35 -0400

    Update eofs test

---
+++ cwt/test/test_timeseries_processing.py
@@ -0,0 +1,31 @@
+import cdms2 as cdms
+import cdutil
+import cdtime
+from plotters import PlotMgr
+import vcs
+import string
+import numpy as np
+
+#===========================================================================================================
+# DATA
+#-----------------------------------------------------------------------------------------------------------
+# Open file ---
+# data_path = 'https://dataserver.nccs.nasa.gov/thredds/dodsC/bypass/CREATE-IP/Reanalysis/NASA-GMAO/GEOS-5/MERRA/mon/atmos/ts.ncml'
+
+data_path = '/dass/pubrepo/CREATE-IP/data/reanalysis/NASA-GMAO/GEOS-5/MERRA2/mon/atmos/ts/ts_Amon_reanalysis_MERRA2_198001-201712.nc'
+f = cdms.open(data_path)
+
+start_year = 1980
+end_year = 2000
+
+start_time = cdtime.comptime(start_year)
+end_time = cdtime.comptime(end_year)
+
+d = f('ts',time=(start_time,end_time),latitude=(-80,80))
+print "Completed data read"
+
+d_anom = cdutil.ANNUALCYCLE.departures(d)
+print "Completed data prep"
+
+plotter = PlotMgr()
+plotter.graph_data( d_anom.data() )
