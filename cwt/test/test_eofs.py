2c48bef499007c7e7bde2cf00f10f9d6cab16e85
ThomasMaxwell <thomas.maxwell@nasa.gov>
2018-06-05 14:24:34 -0400

    Add eofs

--- cwt/test/test_eofs.py
+++ cwt/test/test_eofs.py
@@ -33,7 +33,7 @@ print "Completed data prep"
 solver = Eof( d_anom, weights='none', center=True, scale=True )
 print "Created solver"
 
-eof = solver.eofs( neofs=nModes, eofscaling=1 )
+eof = solver.eofs( neofs=nModes )
 print "Computed eofs"
 
 frac = solver.varianceFraction()
@@ -50,7 +50,7 @@ canvas.setcolormap('bl_to_darkred')
 for iPlot in range(nModes):
     iso = canvas.createisofill()
     level_distribution = np.array( [-1.6, -1.3, -1.0, -0.7, -0.6, -0.5, -0.4, -0.3, -0.2, -0.1, 0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 1.0, 1.3, 1.6] )
-    iso.levels = list( level_distribution*0.01 )
+    iso.levels = list( level_distribution*0.02 )
     iso.ext_1 = 'y' # control colorbar edge (arrow extention on/off)
     iso.ext_2 = 'y' # control colorbar edge (arrow extention on/off)
     cols = vcs.getcolors(iso.levels, range(16,240), split=0)
