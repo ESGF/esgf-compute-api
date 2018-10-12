fd7800fd5e899a4a3f881b76400533771ce8dd78
Jason Boutte <boutte.jason@gmail.com>
2018-10-12 16:36:50 +0000

    Removes lines causes tests to be run twice when using nose2

--- cwt/tests/__init__.py
+++ cwt/tests/__init__.py
@@ -1,9 +1,9 @@
 #! /usr/bin/env python
 """ Testing module. """
 
-import os
-import unittest
-
-suite = unittest.TestLoader().discover(os.path.dirname(__file__))
-
-unittest.TextTestRunner(verbosity=2).run(suite)
+#import os
+#import unittest
+#
+#suite = unittest.TestLoader().discover(os.path.dirname(__file__))
+#
+#unittest.TextTestRunner(verbosity=2).run(suite)
