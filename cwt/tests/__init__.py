79a6553843c99acefeaa5f8df1e0434ca18b9c63
Jason Boutte <boutte.jason@gmail.com>
2018-10-12 18:33:46 +0000

    Removes unused code

--- cwt/tests/__init__.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env python
-""" Testing module. """
-
-#import os
-#import unittest
-#
-#suite = unittest.TestLoader().discover(os.path.dirname(__file__))
-#
-#unittest.TextTestRunner(verbosity=2).run(suite)
