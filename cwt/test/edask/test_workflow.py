84f29641fe65b4a717a0210aa0704c81e8e04ab0
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2018-11-08 10:35:48 -0500

    Update tests

--- cwt/test/edask/test_workflow.py
+++ cwt/test/edask/test_workflow.py
@@ -887,7 +887,7 @@ class TestWorkflow:
 
 if __name__ == '__main__':
     executor = TestWorkflow()
-    executor.test_getCapabilities()
+    executor.cip_high_precip()
 
 #    executor.cip_max_temp()
 #    executor.performance_test_conus_1mth()
