628b28cfb87d39969729d3c644f1aa4133a2a30a
Jason Boutte <boutte.jason@gmail.com>
2018-11-15 16:08:40 -0800

    Updates the build script

--- build_conda.sh
+++ build_conda.sh
@@ -12,8 +12,24 @@ do
     --push)
       PUSH=1
       ;;
+    --git-branch)
+      GIT_BRANCH=$1
+      shift
+      ;;
+    --conda-tag)
+      CONDA_TAG=$1
+      shift
+      ;;
   esac
 done
+
+[ -z "${CONDA_TAG}" ] && echo "Missing required --conda-tag" && exit 1
+
+[ -z "${GIT_BRANCH}" ] && echo "Missing required --git-branch" && exit 1
+
+sed -i.tag.bak "s/\(.*version: \).*/\1\"${CONDA_TAG}\"/" conda/meta.yaml
+
+sed -i.branch.bak "s/\(.*git_rev: \).*/\1${GIT_BRANCH}/" conda/meta.yaml
 
 conda build -c conda-forge --output-folder $PWD/_build conda/
 
@@ -21,7 +37,7 @@ if [[ $PUSH -eq 1 ]]
 then
   anaconda login
 
-  PACKAGE=$(conda build -c conda-forge --output conda/)
+  PACKAGE=$(conda build -c conda-forge --output-folder $PWD/_build --output conda/)
 
-  anaconda upload -c uvcdat -i --force $PACKAGE
+  anaconda upload -u cdat -i $PACKAGE
 fi
