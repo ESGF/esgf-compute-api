ff567440d403cd8003023b110642b8173701075a
Jason Boutte <boutte.jason@gmail.com>
2018-12-02 00:59:55 -0800

    Updates build_conda script

--- build_conda.sh
+++ build_conda.sh
@@ -1,43 +1,49 @@
 #! /bin/bash
 
+function usage() {
+  echo -e "./${0} version build git_branch [--push]"
+}
+
+[[ $# -lt 3 ]] && usage && exit 1
+
+VERSION=$1
+shift
+
+BUILD=$1
+shift
+
+GIT_BRANCH=$1
+shift
+
 PUSH=0
 
 while [[ $# -gt 0 ]]
 do
-  ARG=$1
-
+  NAME=$1
   shift
 
-  case $ARG in
+  case "${NAME}" in
     --push)
       PUSH=1
-      ;;
-    --git-branch)
-      GIT_BRANCH=$1
-      shift
-      ;;
-    --conda-tag)
-      CONDA_TAG=$1
-      shift
       ;;
   esac
 done
 
-[ -z "${CONDA_TAG}" ] && echo "Missing required --conda-tag" && exit 1
+./bump_version.sh ${VERSION} ${GIT_BRANCH} ${BUILD}
 
-[ -z "${GIT_BRANCH}" ] && echo "Missing required --git-branch" && exit 1
-
-sed -i.tag.bak "s/\(.*version: \).*/\1\"${CONDA_TAG}\"/" conda/meta.yaml
-
-sed -i.branch.bak "s/\(.*git_rev: \).*/\1${GIT_BRANCH}/" conda/meta.yaml
-
-conda build -c conda-forge --output-folder $PWD/_build conda/
+conda build -c conda-forge --output-folder ${PWD}/_build conda/
 
 if [[ $PUSH -eq 1 ]]
 then
   anaconda login
 
-  PACKAGE=$(conda build -c conda-forge --output-folder $PWD/_build --output conda/)
+  PACKAGE=$(conda build -c conda-forge --output-folder ${PWD}/_build --output conda/)
 
-  anaconda upload -u cdat -i $PACKAGE
+  conda convert -p osx-64 --output-dir ${PWD}/_build ${PACKAGE}
+
+  anaconda upload -u cdat --force ${PACKAGE}
+
+  PACKAGE_OSX=$(echo ${PACKAGE} | sed "s/linux-64/osx-64/")
+
+  anaconda upload -u cdat --force ${PACKAGE_OSX}
 fi
