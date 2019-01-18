04405fd12f560edb011796c41907c4fd4620e9e0
Jason Boutte <boutte.jason@gmail.com>
2019-01-17 17:23:40 -0800

    Fixes script for bumping the version

--- bump_version.sh
+++ bump_version.sh
@@ -1,21 +1,17 @@
 #! /bin/bash
 
-if [[ $# -lt 2 ]]
+if [[ $# -lt 1 ]]
 then
-  echo -e "Usage: $0 version build"
+  echo -e "Usage: $0 version"
 
   exit 1
 fi
 
 VERSION=$1
 
-BUILD=$3
-
 sed -i "s|\(.*version: \).*|\1\"${VERSION}\"|" ./conda/meta.yaml
 
 sed -i "s|\(.*git_rev: \).*|\1${VERSION}|" ./conda/meta.yaml
-
-sed -i "s|\(.*number: \).*|\1${BUILD}|" ./conda/meta.yaml
 
 sed -i "s|\(__version__ = ).*|\1\'${VERSION}\'|" ./cwt/__init__.py
 
