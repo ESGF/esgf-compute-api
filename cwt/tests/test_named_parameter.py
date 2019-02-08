c325f7fc71833cb7362c811c2e799dba2f2bef74
Jason Boutte <boutte.jason@gmail.com>
2019-02-08 00:46:55 -0800

    Fixes named parameter tests

--- cwt/tests/test_named_parameter.py
+++ cwt/tests/test_named_parameter.py
@@ -7,20 +7,9 @@ import cwt
 class TestNamedParameter(unittest.TestCase):
     """ NamedParameter Test Case. """
 
-    def test_bad_type(self):
-        o = type('X', (object,), dict(test=1))
-
-        p = cwt.NamedParameter('x', o)
-
-        with self.assertRaises(cwt.ParameterError):
-            p.parameterize()
-
     def test_from_string(self):
         p = cwt.NamedParameter.from_string('axes', 'x|y')
 
-        self.assertIsInstance(p.values, list)
+        self.assertIsInstance(p.values, tuple)
         self.assertEqual(len(p.values), 2)
         self.assertItemsEqual(p.values, ['x', 'y'])
-
-if __name__ == '__main__':
-    unittest.main()
