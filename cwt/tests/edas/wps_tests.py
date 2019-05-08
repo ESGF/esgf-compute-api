3695697e619b1e701f6d8297ddec939e0d361c8a
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2019-05-08 12:11:07 -0400

    Add tests

---
+++ cwt/tests/edas/wps_tests.py
@@ -0,0 +1,46 @@
+import cwt, os
+
+TAS = [
+    'https://aims3.llnl.gov/thredds/dodsC/css03_data/CMIP6/CMIP/NASA-GISS/GISS-E2-1-G/amip/r1i1p1f1/Amon/tas/gn/v20181016/tas_Amon_GISS-E2-1-G_amip_r1i1p1f1_gn_185001-190012.nc',
+    'https://aims3.llnl.gov/thredds/dodsC/css03_data/CMIP6/CMIP/NASA-GISS/GISS-E2-1-G/amip/r1i1p1f1/Amon/tas/gn/v20181016/tas_Amon_GISS-E2-1-G_amip_r1i1p1f1_gn_190101-195012.nc',
+    'https://aims3.llnl.gov/thredds/dodsC/css03_data/CMIP6/CMIP/NASA-GISS/GISS-E2-1-G/amip/r1i1p1f1/Amon/tas/gn/v20181016/tas_Amon_GISS-E2-1-G_amip_r1i1p1f1_gn_195101-200012.nc',
+    'https://aims3.llnl.gov/thredds/dodsC/css03_data/CMIP6/CMIP/NASA-GISS/GISS-E2-1-G/amip/r1i1p1f1/Amon/tas/gn/v20181016/tas_Amon_GISS-E2-1-G_amip_r1i1p1f1_gn_200101-201412.nc',
+]
+
+TAS_ESGF= [
+    'esgf@https://dataserver.nccs.nasa.gov/thredds/dodsC/CMIP5/NASA/GISS/historical/E2-H_historical_r2i1p3/clwvi_Amon_GISS-E2-H_historical_r2i1p3_185001-190012.nc'
+    'esgf@https://dataserver.nccs.nasa.gov/thredds/dodsC/CMIP5/NASA/GISS/historical/E2-H_historical_r2i1p3/clwvi_Amon_GISS-E2-H_historical_r2i1p3_190101-195012.nc',
+    'esgf@https://dataserver.nccs.nasa.gov/thredds/dodsC/CMIP5/NASA/GISS/historical/E2-H_historical_r2i1p3/clwvi_Amon_GISS-E2-H_historical_r2i1p3_195101-200512.nc',
+]
+
+SERVER = os.environ.get( "EDAS_HOST_ADDRESS", "https://edas.nccs.nasa.gov/wps/cwt" )
+
+
+
+class wpsTest:
+
+    def __init__(self):
+        self.client = cwt.WPSClient(SERVER)
+        print "Connecting to wps host: " + SERVER
+
+    def cfsr_mth_time_ave(self):
+
+        domain_data = {'name': 'd0', 'lat': {'start': 23.7, 'end': 49.2, 'crs': 'values'},  'lon': {'start': -125, 'end': -70.3, 'crs': 'values'},
+                       'time': {'start': '1980-01-01T00:00:00', 'end': '2016-12-31T23:00:00', 'crs': 'timestamps'}}
+
+        process_data = { 'name': 'xarray.ave',  'input': [ 'v0' ],  'axes': "t",  'domain': "d0",  'result': 'p0' }
+
+        process  = cwt.Process.from_dict( process_data )
+        variable = cwt.Variable( "collection://cip_cfsr_mth", 'tas', name='v0' )
+        domain   = cwt.Domain.from_dict( domain_data )
+
+        self.client.execute( process, [variable], domain )
+        process.wait()
+
+        print "Completed execution, result available at: " + process.output.uri
+
+
+
+if __name__ == '__main__':
+    tester = wpsTest()
+    tester.cfsr_mth_time_ave()
