a1691edad64cc147787b80aad1443b19e40701c9
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2019-05-17 17:46:07 -0400

    Add diagnostics

--- cwt/tests/edas/wps_tests.py
+++ cwt/tests/edas/wps_tests.py
@@ -33,7 +33,7 @@ class wpsTest:
         self.client.execute( process, inputs=[variable], domain=domain, method='get' )
         process.wait()
 
-        print "Completed execution, result available at: " + str(process.context.processOutputs[0].data)
+        print "Completed execution, result available at: " + str(process.context.processOutputs[0].reference)
 
 if __name__ == '__main__':
     tester = wpsTest()
