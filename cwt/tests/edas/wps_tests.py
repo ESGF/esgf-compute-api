f440725ef499f4c604dd0e68a7f09f361dffc675
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2019-05-19 12:12:04 -0400

    Update tests

--- cwt/tests/edas/wps_tests.py
+++ cwt/tests/edas/wps_tests.py
@@ -35,8 +35,7 @@ class wpsTest:
         monitorExecution( process.context, download = True, filepath="/tmp/result-" + str(time.time()) + ".nc" )
 
     def metrics_test(self):
-        process_data = { 'name': 'edas.metrics' }
-        process = cwt.Process.from_dict(process_data)
+        process = self.client.processes('edas.metrics')[0]
         self.client.execute( process, method='get' )
         print json.dumps(process.output, indent=True, sort_keys=True)
 
