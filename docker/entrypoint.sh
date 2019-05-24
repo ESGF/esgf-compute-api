b838161cbea6157e883953decf3c2cf2b4ead387
Jason Boutte <boutte.jason@gmail.com>
2019-05-23 17:03:31 -0700

    Fixes the dockerfile to use multistage builds

--- docker/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-[ ! -z "${PIP_EXTRA_PACKAGES}" ] && pip install ${PIP_EXTRA_PACKAGES}
-
-[ ! -z "${CONDA_EXTRA_PACKAGES}" ] && conda install ${CONDA_EXTRA_PACKAGES}
-
-jupyter lab --ip 0.0.0.0 --port 9999 --allow-root --notebook-dir /esgf-compute-api/examples
