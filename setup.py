2b58fe901a1dde4098ef5da119e17c7d357fa329
Jason Boutte <boutte.jason@gmail.com>
2020-01-27 21:08:05 -0800

    Updates the setup files

--- setup.py
+++ setup.py
@@ -1,17 +1,6 @@
 #!/usr/bin/env python
 
-import os
-import re
 from setuptools import setup
-
-install_path = os.path.dirname(__file__)
-
-ver_path = os.path.join(install_path, 'cwt', '_version.py')
-
-with open(ver_path) as infile:
-    data = infile.readline()
-
-__version__ = re.search('((\\d\\.?)+)', data).group(0)
 
 setup(
     name='esgf-compute-api',
@@ -19,7 +8,7 @@ setup(
     packages=['cwt'],
     author='Jason Boutte',
     author_email='boutte3@llnl.gov',
-    version=__version__,
+    version="2.2.1",
     url='https://github.com/ESGF/esgf-compute-api',
     install_requires=[
         "owslib>=0.17.1",
